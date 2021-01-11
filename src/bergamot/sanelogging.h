175add9bb95b947593318f18f7612a1c49bf7314
jphilip@ed.ac.uk <jphilip@ed.ac.uk>
2021-01-11 21:11:26 +0000

    Adding flush and cppformatting to logging

--- src/bergamot/sanelogging.h
+++ src/bergamot/sanelogging.h
@@ -1,47 +1,43 @@
 #ifndef __BERGAMOT_SANELOGGING_H
 #define __BERGAMOT_SANELOGGING_H
 
-#include <iostream>
 #include "spdlog/spdlog.h"
+#include <iostream>
 
 namespace marian {
 
 #define PLOG(worker, level, ...) checkedPLog(worker, #level, __VA_ARGS__)
 
-
 template <class... Args>
-void checkedPLog(std::string logger, std::string level, Args... args){
+void checkedPLog(std::string logger, std::string level, Args... args) {
   Logger log = spdlog::get(logger);
-  if (!log){
+  if (!log) {
     try {
-      log = spdlog::daily_logger_st(logger, "logs/"+logger+".log");
-    } catch (const spdlog::spdlog_ex& ex) {
+      log = spdlog::daily_logger_st(logger, "logs/" + logger + ".log");
+    } catch (const spdlog::spdlog_ex &ex) {
       std::cout << "Log initialization failed: " << ex.what() << std::endl;
     }
   }
 
-  if(level == "trace")
+  if (level == "trace")
     log->trace(args...);
-  else if(level == "debug")
+  else if (level == "debug")
     log->debug(args...);
-  else if(level == "info")
+  else if (level == "info")
     log->info(args...);
-  else if(level == "warn")
+  else if (level == "warn")
     log->warn(args...);
-  else if(level == "error")
+  else if (level == "error")
     log->error(args...);
-  else if(level == "critical")
+  else if (level == "critical")
     log->critical(args...);
   else {
     log->warn("Unknown log level '{}' for logger '{}'", level, logger);
   }
   // Not required when threads clean-exit.
-  // log->flush();
-
+  log->flush();
 }
 
-
-}  // namespace marian
-
+} // namespace marian
 
 #endif // __BERGAMOT_SANELOGGING_H
